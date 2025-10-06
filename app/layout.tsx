7ac837aaffb0a19e38fb290c6977017bd386f07c
4nh3k <21520579@gm.uit.edu.vn>
2025-10-06 22:45:01 +0700

    Update title and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,14 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Mamden Tracker',
+  description: 'Track scores for Mamden game easily',
+  authors: [{ name: 'Tien Anh'}, { name: 'Tuan'}],
+  icons: {
+    icon: '/favicon.svg',
+    shortcut: '/favicon.svg',
+    apple: '/favicon.svg',
+  },
 }
 
 export default function RootLayout({
